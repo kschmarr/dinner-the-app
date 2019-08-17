d9c6e94fa98f9425a1b00ddb993593032216b78f
Kris Schmarr <kschmarr@gmail.com>
2019-08-18 00:52:55 +0300

    updated api_endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: "https://git.heroku.com/shielded-hollows-75290.git",
+  API_ENDPOINT: "https://shielded-hollows-75290.herokuapp.com/",
   TOKEN_KEY: "dinner-client-auth-token"
 };
