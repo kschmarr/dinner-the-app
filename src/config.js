61496716cc6d890240514bb2c7729950536896b0
Kris Schmarr <kschmarr@gmail.com>
2019-08-18 00:58:51 +0300

    fixed typo in endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: "https://shielded-hollows-75290.herokuapp.com/",
+  API_ENDPOINT: "https://shielded-hollows-75290.herokuapp.com",
   TOKEN_KEY: "dinner-client-auth-token"
 };
